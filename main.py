6a7932759ee23e68cc9f8d84d010f9314c870edf
danielmast <danielmast92@gmail.com>
2022-01-14 12:00:32 +0100

    Clean up code

--- main.py
+++ main.py
@@ -1,48 +1,43 @@
+from datetime import datetime, time
+from typing import List
+
+import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression, RidgeCV
-
-from datetime import *
-
-supply_words = ["pan", "rasp", "kom"]
+from sklearn.linear_model import LinearRegression
 
 
+def load_recipes():
+    return pd.read_csv("data/lunch_recipes.csv", parse_dates=["date"])
 
 
+def process_recipes(recipes, large_dish_indicators=["pan", "rasp", "kom"], ignored_words=["servings", "recipe", "url", "dish"]):
+    for indicator in large_dish_indicators:
+        recipes[indicator] = recipes.recipe.apply(
+            lambda text: tokenize(text).count(indicator) > 0)
 
-def getRecipesDF():
-    df = pd.read_csv("data/lunch_recipes.csv")  # Read lunch recipes dataframe.
-    for wrd in supply_words:
-        def hulp_clean(text):
-            # This function cleans text by seperating all the words and removing punctution
-            #
-            # text:str
+    recipes = recipes.drop(columns=ignored_words)
 
-            str_list = [''.join(O for O in str if O.isalnum()) for str in text.split()]
-            str_list = [str.lower() for str in str_list]
-            return str_list
-
-        df[f"{wrd}"] = df.recipe.apply(lambda text: hulp_clean(text).count(wrd) >0) ## count the amount of times a word occurs in the recipe.
-        df[f"{wrd}"] = df[f"{wrd}"].apply(lambda x: x is True)
-    df = df.drop('servings', axis= 1)
-    df = df.drop('recipe', axis= 1)
-    df['date'] = df.date.apply(lambda x: datetime.strptime(x, '%Y-%m-%d'))
-    df = df.drop("url", axis= 1)
-    df = df.drop("dish", axis= 1)
+    return recipes
 
 
-    return df
+def tokenize(recipe: str) -> List[str]:
+    return [remove_punctuation(token).lower() for token in recipe.split()]
 
-def attendance_sheet_uitlezen():
-    df = pd.read_csv("../clean_code/data/key_tag_logs.csv")
+
+def remove_punctuation(token: str) -> str:
+    return ''.join(character for character in token if character.isalnum())
+
+
+def load_attendances():
+    df = pd.read_csv("data/key_tag_logs.csv")
     df['timestamp2'] = df.timestamp.apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
     df['date'] = df.timestamp.apply(lambda x: datetime.strptime(x[:10], '%Y-%m-%d'))
     df['time'] = df.timestamp2.apply(lambda x: x.time())
     df['timestamp'] = df['timestamp2']
     df = df.drop('timestamp2', axis=1)
-    import numpy as np
+
     result = pd.DataFrame(np.array(df.date), columns=['date']).drop_duplicates()
 
-    # print(df.name.unique())
     for name in df.name.unique():
         lunchdates = []
         for datum in df.date.unique():
@@ -50,35 +45,41 @@ def attendance_sheet_uitlezen():
             df2 = df2[df2.date == datum]
 
             dataframe_check_in = df2[df2.event == "check in"]
-            dataframe_check_in = dataframe_check_in[dataframe_check_in.time < time(12,0, 0)]
+            dataframe_check_in = dataframe_check_in[dataframe_check_in.time < time(12, 0, 0)]
 
             df_check_out = df2[df2.event == "check out"]
             df_check_out = df_check_out[df_check_out.time > time(12, 0, 0)]
             if df_check_out.shape[0] > 0 and dataframe_check_in.shape[0] > 0:
                 lunchdates.append(datum)
 
-        result[f"{name}"] = result.date.apply(lambda x: 1 if x in list(lunchdates) else 0)
+        result[name] = result.date.apply(lambda x: 1 if x in list(lunchdates) else 0)
 
-    result['date'] = result['date']#.apply(str)
     return result
 
-def train_model(alpha=0.1):
-    recipes = getRecipesDF()
-    attendance = attendance_sheet_uitlezen()
-    l=pd.read_csv("data/dishwasher_log.csv")
-    l["date"] = l.date.apply(lambda x: datetime.strptime(x, '%Y-%m-%d'))
+
+def load_dishwasher_log():
+    return pd.read_csv("data/dishwasher_log.csv", parse_dates=['date'])
+
+
+def train_model():
+    recipes = (load_recipes()
+               .pipe(process_recipes))
+    attendance = load_attendances()
+    dishwasher_log = load_dishwasher_log()
 
     df = recipes.merge(attendance,
-                    on="date",
-                       how="outer").merge(l).fillna(0)
-    reg = LinearRegression(fit_intercept=False, positive=True)\
-          .fit(df.drop(["dishwashers", "date"], axis=1),
-            df["dishwashers"])
+                       on="date",
+                       how="outer").merge(dishwasher_log).fillna(0)
+    reg = LinearRegression(fit_intercept=False, positive=True) \
+        .fit(df.drop(["dishwashers", "date"], axis=1),
+             df["dishwashers"])
     return dict(zip(reg.feature_names_in_,
-            [round(c, 3) for c in reg.coef_]))
+                    [round(c, 3) for c in reg.coef_]))
 
+
+def run():
+    print(train_model())
 
 
 if __name__ == "__main__":
-
-    print(train_model())
+    run()
